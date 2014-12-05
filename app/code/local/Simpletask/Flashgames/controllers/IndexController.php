9d9eac22b1bbb4a38764facd1d20ed4cb4153a0b
danielhalupka <daniel.halupka@pbssolutions.net>
2014-12-05 17:05:23 +0100

    fix

--- app/code/local/Simpletask/Flashgames/controllers/IndexController.php
+++ app/code/local/Simpletask/Flashgames/controllers/IndexController.php
@@ -61,7 +61,7 @@ class Simpletask_Flashgames_IndexController extends Mage_Core_Controller_Front_A
 	public function deleteprolorelativeAction(){
 		$itemCollection = Mage::getModel('flashgames/flashgames')->getCollection();
 		foreach($itemCollection as $item) {
-			if (strpos($item->getSwfUrl(), 'barbie') === true){
+			if (strpos($item->getSwfUrl(), 'preloader') === true){
 				echo $item->getId();
 				$item->delete();
 			}
